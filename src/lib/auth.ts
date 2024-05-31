b635cb33ec1bc0453f31a951f8ed5c1f06fcbb7b
hasoy <hamid.soy@hotmail.com>
2024-05-31 12:08:47 +0200

    fix google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -29,7 +29,7 @@ export const loginWithGoogle = async () => {
   signInWithPopup(auth, provider)
     .then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
-      // const credential = GoogleAuthProvider.credentialFromResult(result);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
       // const token = credential.accessToken;
       // The signed-in user info.
       const user = result.user;
