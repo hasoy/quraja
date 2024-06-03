858e023411e609da84788def2e30fa3eeb935b41
hasoy <hamid.soy@hotmail.com>
2024-06-03 20:32:20 +0200

    fix google signin

--- src/forms/resolvers/LoginForm.resolver.ts
+++ src/forms/resolvers/LoginForm.resolver.ts
@@ -1,7 +1,5 @@
 import * as yup from "yup";
-export const loginSchema = yup
-  .object({
-    email: yup.string().email().required(),
-    password: yup.string().required().min(6),
-  })
-  .required();
+export const loginSchema = yup.object({
+  email: yup.string().email().required(),
+  password: yup.string().required().min(6),
+});
